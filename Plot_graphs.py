1953f3ba087b01e3aef329f40bdbde0a5fe3dbfc
SungminYou <43061525+VictorSungminYou@users.noreply.github.com>
2023-06-02 00:23:41 -0400

    Update Plot_graphs.py

--- Plot_graphs.py
+++ Plot_graphs.py
@@ -1,52 +1,107 @@
+from heartrate_monitor import HeartRateMonitor
 import time
-import smbus
+import argparse
 import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib.animation import FuncAnimation
+import smbus
 
-# Constants
+parser = argparse.ArgumentParser(description="Read and print data from MAX30102")
+parser.add_argument("-r", "--raw", action="store_true", default=False,
+                    help="print raw data instead of calculation result")
+parser.add_argument("-t", "--time", type=int, default=30,
+                    help="duration in seconds to read from sensor, default 30")
+args = parser.parse_args()
+
+# Define some constants from the datasheet
 DEVICE     = 0x48 # Device address (A0-A2)
-ADC_CHNLS  = [0x40, 0x41, 0x42, 0x43] # ADC channels
-N_SAMPLES  = 100
+AIN0       = 0x40 # Address of AIN0
+AIN1       = 0x41 # Address of AIN1
+AIN2       = 0x42 # Address of AIN2
+AIN3       = 0x43 # Address of AIN3
 
 bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
 
-# Initialize plot
-plt.ion() 
-fig, ax = plt.subplots(4, 1, sharex=True)
+def readADC(channel):
+    bus.write_byte(DEVICE, channel) # write to device
+    bus.read_byte(DEVICE) # read first byte (we discard this one)
+    return bus.read_byte(DEVICE) # Read second byte and return it
 
-# Initialize data arrays
-data = np.zeros((4, N_SAMPLES))
+# Set up the real-time plot
+fig, ax = plt.subplots()
+x_data, max_data = [], []
+max_line, = ax.plot(x_data, max_data, label="BPM")
 
-def read_adc(channel):
-    """Reads the ADC channel."""
-    if channel in ADC_CHNLS:
-        bus.write_byte(DEVICE, channel)
-        bus.read_byte(DEVICE)  # dummy read to start conversion
-        return bus.read_byte(DEVICE)
-    else:
-        raise ValueError('Invalid ADC channel.')
+ain0_data, ain1_data, ain2_data, ain3_data = [], [], [], []
+ain0_line, = ax.plot(x_data, ain0_data, label="AIN0")
+ain1_line, = ax.plot(x_data, ain1_data, label="AIN1")
+ain2_line, = ax.plot(x_data, ain2_data, label="AIN2")
+ain3_line, = ax.plot(x_data, ain3_data, label="AIN3")
 
-def main():
-    try:
-        while True:
-            for i, channel in enumerate(ADC_CHNLS):
-                value = read_adc(channel)
-                print(f"Channel {channel - 0x40}: {value}")
-                
-                # Shift old data to the left
-                data[i, :-1] = data[i, 1:]
-                # Add new data to the right
-                data[i, -1] = value
-                
-                # Update plot
-                ax[i].clear()
-                ax[i].plot(data[i])
-                ax[i].set_title(f'Channel {channel - 0x40}')
-            
-            plt.pause(0.1)  # short pause for plots to update
+last_bpm = 0
+last_time = 0
 
-    except KeyboardInterrupt:
-        plt.close()
+def update_plot(frame):
+    x_data.append(last_time)
+    max_data.append(last_bpm)
+    
+    # Read the four analog signals from the PCF8591 module
+    ain0 = readADC(AIN0)
+    ain1 = readADC(AIN1)
+    ain2 = readADC(AIN2)
+    ain3 = readADC(AIN3)
+    
+    # Plot the signals
+    ain0_data.append(ain0)
+    ain1_data.append(ain1)
+    ain2_data.append(ain2)
+    ain3_data.append(ain3)
 
-if __name__ == "__main__":
-    main()
+    # Keep only the most recent 1,000 values
+    if len(x_data) > 1000:
+        x_data.pop(0)
+        max_data.pop(0)
+        ain0_data.pop(0)
+        ain1_data.pop(0)
+        ain2_data.pop(0)
+        ain3_data.pop(0)
+
+    max_line.set_xdata(x_data)
+    max_line.set_ydata(max_data)
+    ain0_line.set_xdata(x_data)
+    ain0_line.set_ydata(ain0_data)
+    ain1_line.set_xdata(x_data)
+    ain1_line.set_ydata(ain1_data)
+    ain2_line.set_xdata(x_data)
+    ain2_line.set_ydata(ain2_data)
+    ain3_line.set_xdata(x_data)
+    ain3_line.set_ydata(ain3_data)
+
+    ax.relim()
+    ax.autoscale_view()
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.legend()
+    return max_line, ain0_line, ain1_line, ain2_line, ain3_line,
+
+def PPG_callback(bpm, spo2, time):
+    global last_bpm, last_time
+    last_bpm = bpm
+    last_time = time
+
+print('sensor starting...')
+hrm = HeartRateMonitor(print_raw=args.raw, print_result=True, callback=PPG_callback)
+hrm.start_sensor()
+
+# Set up the animation
+ani = FuncAnimation(fig, update_plot, blit=True, interval=10)
+
+try:
+    plt.show(block=True)
+    time.sleep(args.time)
+except KeyboardInterrupt:
+    print('keyboard interrupt detected, exiting...')
+
+hrm.stop_sensor()
+print('sensor stoped!')   
